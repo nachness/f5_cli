061d7107fb702832713d17cba955e727768d7736
nachness <vv.daskalov@gmail.com>
2018-06-01 12:26:20 +0300

    Make code more consistent and use kwargs to pass argparser

--- f5_modules/f5_cli.py
+++ f5_modules/f5_cli.py
@@ -1,5 +1,6 @@
-from f5_modules.f5_update_controller import F5Manager, NodeController, \
-    VirtualServerController
+from f5_modules.f5_update_controller import (F5Manager, 
+                                             NodeController,
+                                             VirtualServerController)
 from argparse import ArgumentParser
 import getpass
 import sys
@@ -9,17 +10,17 @@ class CommandParser(object):
         
         parser = ArgumentParser(
             description='Tool to modify BIGIP F5 configuration',
-            usage='''f5_cli <command> [<args>]
+             usage='''f5_cli <command> [<args>]
 
-The most commonly used f5_cli commands are:
-   node     Record changes to the repository
-   irule      Download objects and refs from another repository''')
+    The most commonly used f5_cli commands are:
+    node     Record changes to the repository
+    irule      Download objects and refs from another repository''')
         parser.add_argument('command', help='Subcommand to run')
         # parse_args defaults to [1:] for args, but you need to
         # exclude the rest of the args too, or validation will fail
         args = parser.parse_args(sys.argv[1:2])
         if not hasattr(self, args.command):
-            print 'Unrecognized command'
+            print('Unrecognized command')
             parser.print_help()
             sys.exit(1)
         # use dispatch pattern to invoke method with same name
@@ -30,34 +31,34 @@ The most commonly used f5_cli commands are:
         parser = ArgumentParser(
             description='Enable\Disable a backend node')
      
-        parser.add_argument("-u","--user",
-                            help="The admin user of F5",
-                            default="admin", dest="user")
-        parser.add_argument("-p","--password",
-                            help="The admin password for F5, \
-                                  If no pass is specified you'll be prompted",
-                            dest="password")
-        parser.add_argument("-H","--host", help="The IP of F5",
-                            dest="host")
-        parser.add_argument("-P", "--port", type=int,
-                            help="The port on which F5s APIs are served",
-                            default=443, dest="port")
-        parser.add_argument("-N","--name",nargs="*",
-                            help="Node backend to disable, multiple nodes can also\
-                            be specified", dest="nodes_list")
-        parser.add_argument("--disable", action="store_true",
-                            help="Take Disable action towards backend",
-                            dest="disable")
-        parser.add_argument("--enable", action="store_true",
-                            help="Take Enable action towards backend",
-                            dest="enable")
-        parser.add_argument("--partition", dest="partition", default="Common",
-                            help="Specify optional F5 node partition")
+        parser.add_argument('-u','--user',
+                            help='The admin user of F5',
+                            default='admin', dest='user')
+        parser.add_argument('-p','--password',
+                            help='The admin password for F5, \
+                                  If no pass is specified youll be prompted',
+                            dest='password')
+        parser.add_argument('-H','--host', help='The IP of F5',
+                            dest='host')
+        parser.add_argument('-P', '--port', type=int,
+                            help='The port on which F5s APIs are served',
+                            default=443, dest='port')
+        parser.add_argument('-N','--name',nargs='*',
+                            help='Node backend to disable, multiple nodes can also\
+                            be specified', dest='nodes_list')
+        parser.add_argument('--disable', action='store_true',
+                            help='Take Disable action towards backend',
+                            dest='disable')
+        parser.add_argument('--enable', action='store_true',
+                            help='Take Enable action towards backend',
+                            dest='enable')
+        parser.add_argument('--partition', dest='partition', default='Common',
+                            help='Specify optional F5 node partition')
      
         args = parser.parse_args(sys.argv[2:])
 
         if not args.password:
-            args.password = getpass.getpass("F5 Password: ")
+            args.password = getpass.getpass('F5 Password: ')
      
         if args.nodes_list:
             take_node_action(args=args)
@@ -67,37 +68,37 @@ The most commonly used f5_cli commands are:
         parser = ArgumentParser(
             description='Add\Remove an irule')
      
-        parser.add_argument("-u","--user",
-                            help="The admin user of F5",
-                            default="admin", dest="user")
-        parser.add_argument("-p","--password",
-                            help="The admin password for F5, \
-                                  If no pass is specified you'll be prompted",
-                            dest="password")
-        parser.add_argument("-H","--host", help="The IP of F5",
-                            dest="host")
-        parser.add_argument("-P", "--port", type=int,
-                            help="The port on which F5s APIs are served",
-                            default=443, dest="port")
-        parser.add_argument("-V", "--vip", nargs="*",
-                            help="Names of VIPs to modify",
-                            dest="vip_list")
-        parser.add_argument("-N","--name",
-                            help="Names of irules to modify",
-                            dest="rule_name")
-        parser.add_argument("--remove", action="store_true",
-                            help="Remove iRule from a VIP",
-                            dest="remove")
-        parser.add_argument("--add", action="store_true",
-                            help="Add iRule to a VIP",
-                            dest="add")
-        parser.add_argument("--partition", dest="partition", default="Common",
-                            help="Specify optional F5 node partition")
+        parser.add_argument('-u','--user',
+                            help='The admin user of F5',
+                            default='admin', dest='user')
+        parser.add_argument('-p','--password',
+                            help='The admin password for F5, \
+                                  If no pass is specified youll be prompted',
+                            dest='password')
+        parser.add_argument('-H','--host', help='The IP of F5',
+                            dest='host')
+        parser.add_argument('-P', '--port', type=int,
+                            help='The port on which F5s APIs are served',
+                            default=443, dest='port')
+        parser.add_argument('-V', '--vip', nargs='*',
+                            help='Names of VIPs to modify',
+                            dest='vip_list')
+        parser.add_argument('-N','--name',
+                            help='Names of irules to modify',
+                            dest='rule_name')
+        parser.add_argument('--remove', action='store_true',
+                            help='Remove iRule from a VIP',
+                            dest='remove')
+        parser.add_argument('--add', action='store_true',
+                            help='Add iRule to a VIP',
+                            dest='add')
+        parser.add_argument('--partition', dest='partition', default='Common',
+                            help='Specify optional F5 node partition')
      
         args = parser.parse_args(sys.argv[2:])
 
         if not args.password:
-            args.password = getpass.getpass("F5 Password: ")
+            args.password = getpass.getpass('F5 Password: ')
      
 
         if args.rule_name:
@@ -106,40 +107,39 @@ The most commonly used f5_cli commands are:
 def take_node_action(args):
 
     for node in args.nodes_list:
-        node_object = NodeController(node=node,arg_parser=args)
+        nodeController = NodeController(node=node, **args.__dict__)
  
         if args.disable:
-            node_object.take_node_down()
+            nodeController.take_node_down()
 
         elif args.enable:
-            node_object.take_node_up()
+            nodeController.take_node_up()
         else:
-            print("No disable or enable specified, what are you trying to do?")
+            print('No disable or enable specified, what are you trying to do?')
         
-        get_node_status(node_object)
+        get_node_status(nodeController)
 
-def get_node_status(node_object):
+def get_node_status(nodeController):
     
-    session , state = node_object.get_node_status()
-    print("""node session status is: {}
-           node state status is: {}""".format(session, state))
+    session , state = nodeController.get_node_status()
+    print('''node session status is: {}
+           node state status is: {}'''.format(session, state))
 
 def take_rules_action(args):
-
+    print(args.__dict__) 
     for vs_name in args.vip_list:
-        vs_object = VirtualServerController(vip_name=vs_name,
-                                            arg_parser=args)
+        virtualServerController = VirtualServerController(vip_name=vs_name,
+            **args.__dict__)
  
         if args.remove:
-            vs_object.remove(args.rule_name)
+            virtualServerController.remove(args.rule_name)
         elif args.add:
-            vs_object.add(args.rule_name)
+            virtualServerController.add(args.rule_name)
         else:
-            print("No add or remove specified, what are you trying to do?")
+            print('No add or remove specified, what are you trying to do?')
         
-        print vs_object
-        rules_list = vs_object.get_vip_attr("rules")
-        print("Current active rules are %s" % (rules_list))
+        rules_list = virtualServerController.get_vip_attr('rules')
+        print('Current active rules are %s' % (rules_list))
 
 def main():
     CommandParser()
--- f5_modules/f5_update_controller.py
+++ f5_modules/f5_update_controller.py
@@ -3,31 +3,32 @@ from icontrol.exceptions import iControlUnexpectedHTTPError
 
 class F5Manager(object):
     
-    def __init__(self, arg_parser):
-        self.hostname = arg_parser.host
-        self.admin = arg_parser.user
-        self.password = arg_parser.password
-        self.port = arg_parser.port
+    def __init__(self, **kwargs):
+        print(kwargs)
+        self.hostname = kwargs.pop('host')
+        self.admin = kwargs.pop('user')
+        self.password = kwargs.pop('password')
+        self.port = kwargs.pop('port')
         self.f5_manager = None
 
     def connect(self):
-        print("Trying to connect to {}".format(self.hostname))
+        print('Trying to connect to {}'.format(self.hostname))
         try:
             self.f5_manager = ManagementRoot(hostname=self.hostname,
                                        username=self.admin,
                                        password=self.password,
                                        port=self.port)
-            print("Connected successfully")
+            print('Connected successfully')
         except iControlUnexpectedHTTPError as err:
             raise 
 
 
 class NodeController(F5Manager):
     
-    def __init__(self, node_name, partition, arg_parser):
-        super(F5Manager,self).__init__(arg_parser)
+    def __init__(self, node_name, **kwargs):
+        super(F5Manager,self).__init__(**kwargs)
         self.node_name = node_name
-        self.partition = arg_parser.partition
+        self.partition = kwargs.pop('partition')
         try:
             self.f5_manager.connect()
             self.node_object = self.f5_manager.tm.ltm.nodes.node.load(
@@ -38,17 +39,17 @@ class NodeController(F5Manager):
 
     def take_node_down(self):
         try:
-            print("Disabling node {}".format(self.node_name))
-            self.node_object.modify(session="user-disabled", state="user-down")
-            print("Disabled node {}".format(self.node_name))
+            print('Disabling node {}'.format(self.node_name))
+            self.node_object.modify(session='user-disabled', state='user-down')
+            print('Disabled node {}'.format(self.node_name))
         except Exception:
             raise 
 
     def take_node_up(self):
         try: 
-            print("Enabling node {}".format(self.node_name))
-            self.node_object.modify(session="user-enabled", state="user-up")
-            print("Enabled node {}".format(self.node_name))
+            print('Enabling node {}'.format(self.node_name))
+            self.node_object.modify(session='user-enabled', state='user-up')
+            print('Enabled node {}'.format(self.node_name))
         except Exception:
             raise 
 
@@ -57,11 +58,11 @@ class NodeController(F5Manager):
 
 class VirtualServerController(F5Manager):
     
-    def __init__(self, vip_name, arg_parser):
-        super(VirtualServerController, self).__init__(arg_parser)
+    def __init__(self, vip_name, **kwargs):
+        super(VirtualServerController, self).__init__(**kwargs)
         super(VirtualServerController, self).connect()
         self.vip_name = vip_name
-        self.partition = arg_parser.partition
+        self.partition = kwargs.pop('partition')
         try:
             self.vip_object = self.f5_manager.tm.ltm.virtuals.virtual.load(
                 name=self.vip_name,
@@ -71,7 +72,7 @@ class VirtualServerController(F5Manager):
 
     def __refresh(self):
         try: 
-            print("Reloading %s virtualserver" % self.vip_name)
+            print('Reloading %s virtualserver' % self.vip_name)
             self.vip_object.refresh()
         except Exception as err:
             raise
@@ -79,26 +80,26 @@ class VirtualServerController(F5Manager):
     def add(self, irule_name):
         try:
             if irule_name not in self.vip_object.rules:
-                print("Adding rule %s" % (irule_name))
+                print('Adding rule %s' % (irule_name))
                 self.vip_object.rules.append(irule_name)
                 self.vip_object.update('rules')           
-                print("Rule added")
+                print('Rule added')
                 self.__refresh()
             else:
-                print("Rule already exists") 
-        except Exception:
+                print('Rule already exists') 
+        except Exception as err:
             raise 
 
     def remove(self, irule_name):
         try: 
             if irule_name in self.vip_object.rules:
-                print("Enabling node %s" % (self.irule_name))
+                print('Enabling node %s' % (self.irule_name))
                 self.vip_object.rules.remove(irule_name)
                 self.vip_object.update('rules')
-                print("Rule removed")
+                print('Rule removed')
                 self.__refresh()
             else:
-                print("iRule is not present on this VS")
+                print('iRule is not present on this VS')
         except Exception:
             raise 
 
